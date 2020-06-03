e4d846080872c3c324edfef6563336297f83007c
talvane <talvane.magalhaes@gmail.com>
2020-06-03 10:17:07 -0300

    added story and stories

--- src/containers/Stories/Stories.jsx
+++ src/containers/Stories/Stories.jsx
@@ -5,16 +5,55 @@ import Story from '../../components/Story';
 import './Stories.scss';
 
 const Stories = ({ stories, getUserHandler }) => {
+  const [showStory, setShowStory] = useState(false);
+  const [selectedStory, setSelectedHistory] = useState({});
+  const [selectedProfile, setSelectedProfile] = useState({});
+
+  const findStoryById = (id) => stories.find((story) => story.id === id);
+
+  const handleStory = (story) => {
+    const foundStory = findStoryById(story.id);
+    const profileData = getUserHandler(story.userId);
+
+    setSelectedProfile(profileData);
+    setSelectedHistory(foundStory);
+    setShowStory(!showStory);
+  };
+
   return (
-    <React.Fragment>
-      <section className="stories">
-        <div className="container"></div>
+    <>
+      <section className="stories" data-testid="stories">
+        <div className="container">
+          {stories.map((story, index) => {
+            const profileData = getUserHandler(story.userId);
+
+            return (
+              <button
+                key={story.id}
+                onClick={() => handleStory(story)}
+                className={`user__thumb ${
+                  index === 0 && 'user__thumb--hasNew'
+                }`}
+              >
+                <div className="user__thumb__wrapper">
+                  {profileData?.avatar && (
+                    <img src={profileData?.avatar} alt={profileData.name} />
+                  )}
+                </div>
+              </button>
+            );
+          })}
+        </div>
       </section>
 
-      {/*showStory && (
-        <Story />
-      )*/}
-    </React.Fragment>
+      {showStory && (
+        <Story
+          story={selectedStory}
+          user={selectedProfile}
+          handleClose={() => setShowStory(!showStory)}
+        />
+      )}
+    </>
   );
 };
 
